baf77942675cef096038049ca6a62ba7907b5c9a
Affan <hahaffan@Affans-MacBook-Air.local>
2026-06-20 23:13:54 +0530

    message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
             System.out.print("$ ");
             String input = scanner.nextLine();
 
-            if (input.equals("exit 0")) {
+            if (input.equals("exit")) {
                 break;
             }
 
@@ -20,9 +20,9 @@ public class Main {
             if (input.startsWith("type ")) {
                 String command = input.substring(5);
 
-                if (command.equals("echo") ||
-                    command.equals("exit") ||
-                    command.equals("type")) {
+                if (command.equals("echo")
+                        || command.equals("exit")
+                        || command.equals("type")) {
                     System.out.println(command + " is a shell builtin");
                 } else {
                     System.out.println(command + ": not found");
@@ -33,5 +33,7 @@ public class Main {
 
             System.out.println(input + ": command not found");
         }
+
+        scanner.close();
     }
 }

335f57cf29bcbd44f2ad50b1e967371210189eac
Affan <hahaffan@Affans-MacBook-Air.local>
2026-06-20 22:50:11 +0530

    message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("$ ");
+            String command = sc.nextLine();
+
+            System.out.println(command + ": command not found");
+        }
     }
 }
